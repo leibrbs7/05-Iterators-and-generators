c75cde6608dce95212dd6acf43a476140696e7e9
Leila Jorg Barbosa <leilajorg.barbosa@leilajorg.barbosa-C57CTF6Y06>
2022-11-22 13:19:44 +0800

    iterable and generator fibo

--- generator.js
+++ generator.js
@@ -1,15 +1,18 @@
 const number = process.argv[2]
 
 function createGenerator(start = 0, end = number, step = 1) {
+    let start = 0
     let value = start
+    let n2 = 1
+
     return function* generatorFunction() {
         let result, nextTerm
         if (value < end){
             for (let i = 1; i <= end; i++){
                 result = { value, done: false }
-                nextTerm = value + step
-                value = step
-                step = nextTerm
+                nextTerm = value + n2
+                value = n2
+                n2 = nextTerm
                 yield result
             }
         } return { value, done: true }
--- iterable.js
+++ iterable.js
@@ -1,18 +1,20 @@
 const number = process.argv[2]
 
-function createIteratable(start = 0, end = number, step = 1) {
+function createIteratable(end = number) {
+    let start = 0
     let value = start
+    let n2 = 1
     return {
       [Symbol.iterator]: function () {
         return {
           next() {
             let result, nextTerm
             if (value < end) {
-              for (let i = 1; i <= end; i++){
+              for (let i = 1; i <= end; i++){ 
                 result = { value, done: false }
-                nextTerm = value + step
-                value = step
-                step = nextTerm
+                nextTerm = value + n2
+                value = n2
+                n2 = nextTerm
                 return result
               }
             } return { value, done: true }
