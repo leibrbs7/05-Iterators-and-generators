3a08650f172bfebb993a9157f3aaa20176b0376f
Leila Jorg Barbosa <leilajorg.barbosa@leilajorg.barbosa-C57CTF6Y06>
2022-11-22 10:16:44 +0800

    iterable-fibo

---
+++ iterable.js
@@ -0,0 +1,26 @@
+const number = process.argv[2]
+
+function createIteratable(start = 0, end = number, step = 1) {
+    let value = start
+    return {
+      [Symbol.iterator]: function () {
+        return {
+          next() {
+            let result, nextTerm
+            if (value < end) {
+              for (let i = 1; i <= end; i++){
+                result = { value, done: false }
+                nextTerm = value + step
+                value = step
+                step = nextTerm
+                return result
+              }
+            } return { value, done: true }
+          },
+        }
+      },
+    }
+}
+
+const iterable = createIteratable()
+console.log(...iterable)
