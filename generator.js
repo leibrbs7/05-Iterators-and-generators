a5be5a987b67b5ec190d5711f903ef93a30b6204
Leila Jorg Barbosa <leilajorg.barbosa@leilajorg.barbosa-C57CTF6Y06>
2022-11-22 14:11:47 +0800

    cosmetic fixes

--- generator.js
+++ generator.js
@@ -1,19 +1,20 @@
 function createGenerator(end) {
-    let value = 0
-    let n2 = 1
+  let value = 0
+  let n2 = 1
 
-    return function* generatorFunction() {
-        let result, nextTerm
-        if (value < end){
-            for (let i = 1; i <= end; i++){
-                result = { value, done: false }
-                nextTerm = value + n2
-                value = n2
-                n2 = nextTerm
-                yield result
-            }
-        } return { value, done: true }
-    }
+  return function* generatorFunction() {
+    let result; let
+      nextTerm
+    if (value < end) {
+      for (let i = 1; i <= end; i += 1) {
+        result = { value, done: false }
+        nextTerm = value + n2
+        value = n2
+        n2 = nextTerm
+        yield result
+      }
+    } return { value, done: true }
+  }
 }
 
 const generatorFunction = createGenerator(10)
@@ -24,7 +25,7 @@ console.log([...generatorObject])
 const generatorFunction1 = createGenerator(10)
 const generatorObject1 = generatorFunction1()
 
-for (let i = 0; i < 6; i++) {
-    console.log(generatorObject1.next())
-    console.log(generatorObject1.next())
+for (let i = 0; i < 6; i += 1) {
+  console.log(generatorObject1.next())
+  console.log(generatorObject1.next())
 }
--- iterable.js
+++ iterable.js
@@ -1,22 +1,23 @@
 function createIteratable(end) {
   let value = 0
   let n2 = 1
-    return {
-      [Symbol.iterator]: function () {
-        return {
-          next() {
-            let result, nextTerm
-            if (value < end) {
-                result = { value, done: false }
-                nextTerm = value + n2
-                value = n2
-                n2 = nextTerm
-                return result
-            } return { value, done: true }
-          },
-        }
-      },
-    }
+  return {
+    [Symbol.iterator]() {
+      return {
+        next() {
+          let result; let
+            nextTerm
+          if (value < end) {
+            result = { value, done: false }
+            nextTerm = value + n2
+            value = n2
+            n2 = nextTerm
+            return result
+          } return { value, done: true }
+        },
+      }
+    },
+  }
 }
 
 const iterable2 = createIteratable(10)
